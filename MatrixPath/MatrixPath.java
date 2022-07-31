ff2b203fc91076f3430b439249a59acdbd07c3bf
Shivam <shivam123ortan@gmail.com>
2022-07-31 12:19:04 +0530

    Added MatrixPath

---
+++ MatrixPath/MatrixPath.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class MatrixPath {
+
+    static int count(int m, int n){
+        if (m==1 || n==1){
+            return 1;
+        }
+        return count(m-1, n) + count(m, n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        System.out.println(count(m, n));
+    }    
+}
